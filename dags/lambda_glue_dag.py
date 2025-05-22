96eb2e2a077bcabfd9d678f0dfb275ef2400d2bb
sattyamghumare <80090444+sattyamghumare@users.noreply.github.com>
2025-05-22 16:34:27 +0530

    Create lambda_glue_dag.py

---
+++ dags/lambda_glue_dag.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
+from airflow.utils.dates import days_ago
+ 
+with DAG(
+    dag_id="aws_services_etl_dag",
+    schedule_interval=None,
+    start_date=days_ago(1),
+    catchup=False,
+    tags=["aws", "lambda", "glue"]
+) as dag:
+ 
+    invoke_lambda = LambdaInvokeFunctionOperator(
+        task_id="invoke_lambda_function",
+        function_name="lambda_bank_etl",
+        aws_conn_id="aws_conn_sattyamg"
+    )
+ 
+    run_glue = GlueJobOperator(
+        task_id="run_glue_job",
+        job_name="glue_bank_transformation",
+        aws_conn_id="aws_default",
+        region_name="us-east-1"
+    )
+ 
+    invoke_lambda >> run_glue
+ 
